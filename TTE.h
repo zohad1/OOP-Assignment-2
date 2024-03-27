3c2659a5838436b463d68301ff1e9983e5ad3b93
PR0G_GRAM3D <79372247+Armaghan195@users.noreply.github.com>
2024-03-28 00:07:18 +0500

    Update TTE.h

--- TTE.h
+++ TTE.h
@@ -3,16 +3,6 @@
 
 #include <string>
 
-class TimetableEntry {
-public:
-    std::string subject;
-    std::string teacher;
-    std::string room;
-    std::string section;
-    std::string student;
-    std::string time;
 
-    TimetableEntry(std::string sub, std::string tea, std::string rom, std::string sec, std::string stu, std::string tim);
-};
 
 #endif
