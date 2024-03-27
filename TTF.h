7fe3c0116d72cb508ad2bbdd57c969ab7c217751
PR0G_GRAM3D <79372247+Armaghan195@users.noreply.github.com>
2024-03-28 00:03:02 +0500

    Update TTF.h

--- TTF.h
+++ TTF.h
@@ -10,7 +10,7 @@ std::vector<TimetableEntry> getTimetableForDay(const std::vector<TimetableEntry>
 std::vector<TimetableEntry> getTimetableForTime(const std::vector<TimetableEntry> &timetable, const std::string &day, const std::string &time);
 std::vector<TimetableEntry> getTimetableForTeacher(const std::vector<TimetableEntry>& timetable, const std::string& teacher);
 std::vector<TimetableEntry> getTimetableForSection(const std::vector<TimetableEntry> &timetable, const std::string &section);
-
+std::vector<TimetableEntry> getTimetableForRoom(const std::vector<TimetableEntry> &timetable, const std::string &room);
 std::vector<TimetableEntry> getTimetableForStudent(const std::vector<TimetableEntry> &timetable, const std::string &student);
 
 #endif
